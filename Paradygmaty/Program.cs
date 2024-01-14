20a0fd6388e54ab39beea472702e9296cde9fa12
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2024-01-14 21:44:33 +0100

    End of lab1

--- Paradygmaty/Program.cs
+++ Paradygmaty/Program.cs
@@ -1,5 +1,16 @@
 ﻿using Paradygmaty.model;
 
-Client c1 = new Client();
+/*
+ Client c1 = new Client();
+c1.setFirstName("Jan");
+c1.setLastName("Nowak");
+c1.setPersonalID("22222222222");
+
+Console.WriteLine(c1.getClinetInfo());
+*/
+
+Client c2 = new Client("Jan", "Kowalski", "11111111116");
+
+Console.WriteLine(c2.getClinetInfo());
 
 //Console.WriteLine();
--- Paradygmaty/model/Client.cs
+++ Paradygmaty/model/Client.cs
@@ -2,8 +2,79 @@
 
 public class Client
 {
+    private string firstName;
+    private string lastName;
+    private readonly string personalID;
     public Client()
     {
         Console.WriteLine("Wywołanie konstruktora bezparametrowego");
+    }
+
+    public Client(string firstName, string lastName, string personalId)
+    {
+        setFirstName(firstName);
+        setLastName(lastName);
+        if (IsValidPersonalID(personalId))
+        {
+            this.personalID = personalId;
+        }
+        else
+        {
+            throw new ArgumentException("Nieprawidłowy numer PESEL");
+        }
+        Console.WriteLine("Wywołano konstruktor z parametrami");
+    }
+    
+    public string getClinetInfo()
+    {
+        return $"Kinent imię {firstName} {lastName}, pesel: {personalID}";
+    }
+
+    public void setFirstName(string firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public string getFirstName()
+    {
+        return this.firstName;
+    }
+    
+    public void setLastName(string lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public string getLastName(string lastName)
+    {
+        return this.lastName;
+    }
+
+    /*public void setPersonalID(string personalId)
+    {
+        this.personalID = personalId;
+    }*/
+
+    public string getPersonalID()
+    {
+        return this.personalID;
+    }
+
+    private bool IsValidPersonalID(string personalID)
+    {
+        if (string.IsNullOrEmpty(personalID) || personalID.Length != 11 || !personalID.All(char.IsDigit))
+        {
+            return false;
+        }
+
+        int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1 };
+        int sum = 0;
+
+        for (int i = 0; i < 11; i++)
+        {
+            sum += (personalID[i] - '0') * weights[i];
+        }
+
+        return sum % 10 == 0;
     }
 }
